c7933eeca4d0eca7bd0547459ccd6a8df083a259
Shaco216 <p.griffin.1911@web.de>
2024-02-24 23:05:11 +0100

    person done

--- HaushaltsManager/MainWindow.xaml.cs
+++ HaushaltsManager/MainWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.IO;
-using System.Windows.Controls.Primitives;
-using System.Xml.Linq;
 using HaushaltsManager.Repository;
 using HaushaltsManager.DBCreator;
 using HaushaltsManager.Model;
--- HaushaltsManager/PersonOptions.xaml.cs
+++ HaushaltsManager/PersonOptions.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using HaushaltsManager.Model;
 using HaushaltsManager.Repository;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace HaushaltsManager
 {
@@ -21,7 +9,7 @@ namespace HaushaltsManager
     /// </summary>
     public partial class PersonOptions : Window
     {
-        private readonly BasicRepository basicRepository;
+        private readonly BasicRepository rep;
 
         public PersonOptions()
         {
@@ -31,36 +19,47 @@ namespace HaushaltsManager
         public PersonOptions(BasicRepository basicRepository)
         {
             InitializeComponent();
-            this.basicRepository = basicRepository;
+            rep = basicRepository;
             OnLoad();
         }
 
         private void InsertPerson_Click(object sender, RoutedEventArgs e)
         {
             int highestId = ((IEnumerable<Person>)LocatedPerson.ItemsSource).Max(x => x.Id);
-            AddPerson addPerson = new AddPerson(basicRepository,highestId,this);
+            AddPerson addPerson = new AddPerson(rep,highestId,this);
             addPerson.Show();
         }
 
         private void UpdatePerson_Click(object sender, RoutedEventArgs e)
         {
-
+            if(LocatedPerson.SelectedItem != null)
+            {
+                Person selectedperson = LocatedPerson.SelectedItem as Person;
+                rep.DoNonQueryCommand(SQLStatementProvider.UpdatePerson.Replace("@Vorname", selectedperson.Vorname).Replace("@Nachname", selectedperson.Nachname)
+                    .Replace("@Id", selectedperson.Id.ToString()));
+                OnLoad();
+            }
         }
 
         private void DeletePerson_Click(object sender, RoutedEventArgs e)
         {
-
+            if (LocatedPerson.SelectedItem != null)
+            {
+                Person selectedperson = LocatedPerson.SelectedItem as Person;
+                rep.DoNonQueryCommand(SQLStatementProvider.DeletePerson.Replace("@Id", selectedperson.Id.ToString()));
+                OnLoad();
+            }
         }
 
         private void OpenEinkommenOptionen_Click(object sender, RoutedEventArgs e)
         {
-            EinkommenOptionen einkommenOptionen = new EinkommenOptionen(basicRepository);
+            EinkommenOptionen einkommenOptionen = new EinkommenOptionen(rep);
             einkommenOptionen.Show();
         }
 
         public void OnLoad()
         {
-            LocatedPerson.ItemsSource = basicRepository.DoQueryCommand<IEnumerable<Person>>(SQLStatementProvider.GatherPerson);
+            LocatedPerson.ItemsSource = rep.DoQueryCommand<IEnumerable<Person>>(SQLStatementProvider.GatherPerson);
         }
     }
 }
