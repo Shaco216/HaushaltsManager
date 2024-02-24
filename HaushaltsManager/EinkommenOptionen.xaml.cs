377caf3df8aca5f8c0db58049ad892f23cd7a02a
Shaco216 <p.griffin.1911@web.de>
2024-02-24 23:31:03 +0100

    einkommen angefangne

--- HaushaltsManager/EinkommenOptionen.xaml.cs
+++ HaushaltsManager/EinkommenOptionen.xaml.cs
@@ -1,4 +1,5 @@
-﻿using HaushaltsManager.Repository;
+﻿using HaushaltsManager.Model;
+using HaushaltsManager.Repository;
 using System.Windows;
 
 namespace HaushaltsManager
@@ -18,6 +19,41 @@ namespace HaushaltsManager
         public EinkommenOptionen(BasicRepository rep)
         {
             this.rep = rep;
+            LoadPersons();
+        }
+
+        public void LoadEinkommenFromPerson() 
+        {
+            if(LocatedPerson.SelectedItem != null)
+            {
+                Person selectedPerson = LocatedPerson.SelectedItem as Person;
+                ClickedPersonToEinkommen.ItemsSource = rep.DoQueryCommand<IEnumerable<Einkommen>>(SQLStatementProvider.GatherEinkommenFromPerson.Replace("@PersonId", selectedPerson.Id.ToString()));
+            }
+        }
+
+        public void LoadPersons()
+        {
+            LocatedPerson.ItemsSource = rep.DoQueryCommand<IEnumerable<Person>>(SQLStatementProvider.GatherPerson);
+        }
+
+        private void InsertEinkommen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UpdateEinkommen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DeleteEinkommen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void LocatedPerson_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            LoadEinkommenFromPerson();
         }
     }
 }
