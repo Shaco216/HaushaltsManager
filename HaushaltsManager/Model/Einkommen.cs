e323767d577ff5df741da3a8a48507cbd815fe0e
Shaco216 <p.griffin.1911@web.de>
2024-02-24 21:12:21 +0100

    sqlstatements angepasst

---
+++ HaushaltsManager/Model/Einkommen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HaushaltsManager.Model
+{
+    public class Einkommen
+    {
+        public long Id { get; set; }
+        public int PersonId { get; set; }
+        public int Jahr {  get; set; }
+        public string Name { get; set; }
+        public double Wert { get; set; }
+        public int KategorieId { get; set; }
+    }
+}
